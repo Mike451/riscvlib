acda98964ee82f579cc4ddeefab0f25effb1a267
Mike Harris <mike.harris451@gmail.com>
2025-03-14 11:32:52 -0700

    fixd several pseudos, added test

--- tests/test_instructions.py
+++ tests/test_instructions.py
@@ -1,5 +1,5 @@
 import unittest
-from riscvlib.instruction import Instruction
+from riscvlib.instruction import Instruction, translate_pseudo_instruction, parse_riscv_instruction_line
 
 
 class TestInstructions(unittest.TestCase):
@@ -7,7 +7,7 @@ class TestInstructions(unittest.TestCase):
     Test known good bit patterns
     https://luplab.gitlab.io/rvcodecjs/#q=sub+x1,+x15,+x7&abi=false&isa=AUTO
     """
-    def test_R_selection(self):
+    def test_R_instructions(self):
         # sub
         i = Instruction.from_line("sub x1, x15, x7")
         self.assertEqual("01000000011101111000000010110011", i.to_bitstring())
@@ -16,7 +16,6 @@ class TestInstructions(unittest.TestCase):
         i = Instruction.from_line("and x1, a0, x5")
         self.assertEqual("00000000010101010111000010110011", i.to_bitstring())
 
-        # sll x1, x2, x3    locical - backfill with 0
         i = Instruction.from_line("sll x1, x2, x3")
         self.assertEqual("00000000001100010001000010110011", i.to_bitstring())
 
@@ -37,7 +36,6 @@ class TestInstructions(unittest.TestCase):
         # hex immediate also neg
         i = Instruction.from_line("ori s0, x12, 0xFED")
         self.assertEqual("11111110110101100110010000010011", i.to_bitstring())
-
 
     def test_IL_instructions(self):
         # type IL in the assembler, loads
@@ -64,7 +62,7 @@ class TestInstructions(unittest.TestCase):
         self.assertEqual("00000101001000110010010010110111", i.to_bitstring())
 
         # negative looking  hex number (treated as unsigned)
-        i = Instruction.from_line("lui s0, 0xFADCE") # load upper immediate
+        i = Instruction.from_line("lui s0, 0xFADCE")  # load upper immediate
         self.assertEqual("11111010110111001110010000110111", i.to_bitstring())
 
         # int
@@ -127,3 +125,34 @@ class TestInstructions(unittest.TestCase):
         # neg
         i = Instruction.from_line("jal x0, -1000000") # back wards as in J .loop_start
         self.assertEqual("11011100000100001011000001101111", i.to_bitstring())
+
+
+class TestPseudoInstructions(unittest.TestCase):
+
+    def test_selection(self):
+        test_data = [
+            ("li x1, 55", ["addi x1, x0, 55"]),
+            ("mv a0, x3", ["addi a0, x3, 0"]),
+            ("li x1, 55", ["addi x1, x0, 55"]),
+            ("nop", ["addi x0, x0, 0"]),
+            ("not x3, x4", ["xori x3, x4, -1"]),
+            ("neg x5, x7", ["sub x5, x0, x7"]),
+            ("j -40", ["jal x0, -40"]),
+            ("ret", ["jalr x0, x1, 0"]),
+            ("call 4000", ["jal x1, 4000"]),
+            ("bnez x5, -60", ["bne x5, x0, -60"]),
+            ("ble x1, x2, 44", ["bge x2, x1, 44"]),
+            ("bgt x1, x2, 60", ["blt x2, x1, 60"]),
+            ("bgtu x1, x2, 64", ["bltu x2, x1, 64"]),
+            ("bleu x1, x2, -68", ["bgeu x2, x1, -68"]),
+        ]
+
+        for tup in test_data:
+            m, args = parse_riscv_instruction_line(tup[0])
+            out = translate_pseudo_instruction(m, *args)
+            self.assertEqual(tup[1], out, f"Failed on '{tup[0]}'")
+
+    def test_pseudo_expands_multiple_instructs(self):
+        # 'la' expands to 2 instructions
+        out = translate_pseudo_instruction("la", "x3", "4000")
+        self.assertEqual(2, len(out))
